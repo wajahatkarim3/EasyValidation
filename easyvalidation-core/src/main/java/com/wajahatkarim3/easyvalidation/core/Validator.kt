3420d0d25b32d566d2bdffdd92dd4b813115a2d8
Wajahat Karim <wajahatkarim3@gmail.com>
2018-05-08 18:21:11 +0500

    added dependencies in kotlin way and empty validator class

---
+++ easyvalidation-core/src/main/java/com/wajahatkarim3/easyvalidation/core/Validator.kt
@@ -0,0 +1,20 @@
+package com.wajahatkarim3.easyvalidation.core
+
+/**
+ * The core Validator builder class for validation operations and checks!
+ *
+ * This class allows developers to process single or multiple validation checks on input views.
+ *
+ * @author Wajahat Karim
+ * @date 08/05/18
+ */
+class Validator
+{
+    /*
+     * Boolean to determine whether all the validations have passed successfully!
+     * If any validation check is failed, then the value to
+     * false and result is returned to developer
+     */
+    private var isValid = true
+
+}
