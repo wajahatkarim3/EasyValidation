13c02d03ad5f218d4a2b2e27ef1acf02429c3518
Emre Köse <e.kose26@gmail.com>
2018-10-10 16:08:50 +0300

    added error messages some rule classes

--- easyvalidation-core/src/main/java/com/wajahatkarim3/easyvalidation/core/rules/AllLowerCaseRule.kt
+++ easyvalidation-core/src/main/java/com/wajahatkarim3/easyvalidation/core/rules/AllLowerCaseRule.kt
@@ -1,15 +1,19 @@
 package com.wajahatkarim3.easyvalidation.core.rules
 
-import com.wajahatkarim3.easyvalidation.core.Validator
+import android.support.annotation.StringRes
 
 /**
  * Returns false if atleast one or more characters are upper case
  *
  * @author Wajahat Karim
  */
-class AllLowerCaseRule : BaseRule
+class AllLowerCaseRule(@StringRes var errorMsg: String = "All letters should be in lower case.") : BaseRule
 {
     override fun validate(text: String): Boolean = text == text.toLowerCase()
 
-    override fun getErrorMessage(): String = "All letters should be in lower case."
+    override fun getErrorMessage(): String = errorMsg
+
+    override fun setError(msg: String) {
+        errorMsg = msg
+    }
 }
--- easyvalidation-core/src/main/java/com/wajahatkarim3/easyvalidation/core/rules/AllUpercCaseRule.kt
+++ easyvalidation-core/src/main/java/com/wajahatkarim3/easyvalidation/core/rules/AllUpercCaseRule.kt
@@ -1,15 +1,19 @@
 package com.wajahatkarim3.easyvalidation.core.rules
 
-import com.wajahatkarim3.easyvalidation.core.Validator
+import android.support.annotation.StringRes
 
 /**
  * Returns false if atleast one or more characters are lower case
  *
  * @author Wajahat Karim
  */
-class AllUpercCaseRule : BaseRule
+class AllUpercCaseRule(@StringRes var errorMsg: String = "All letters should be in upper case.") : BaseRule
 {
     override fun validate(text: String): Boolean = text == text.toUpperCase()
 
-    override fun getErrorMessage(): String = "All letters should be in upper case."
+    override fun getErrorMessage(): String = errorMsg
+
+    override fun setError(msg: String) {
+        errorMsg = msg
+    }
 }
